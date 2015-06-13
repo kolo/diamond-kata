20e2868909730af6345de91bd4d07d1d2e412ad6
codetorial <git.codetorial@gmail.com>
2015-06-13 23:07:05 +0200

    initial commit

---
+++ kata.rb
@@ -0,0 +1,5 @@
+class Kata
+
+  # code here
+
+end
---
+++ spec.rb
@@ -0,0 +1,9 @@
+require_relative 'kata'
+require 'minitest/autorun'
+require 'minitest/spec'
+
+class KataSpec < Minitest::Spec
+
+  # start testing here
+
+end
